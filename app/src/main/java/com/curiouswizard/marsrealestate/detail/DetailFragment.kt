f884652e3ca1dc990263749f29e83fa5b84f4eef
CuriousWizard <tremmel.david@hotmail.com>
2021-03-25 19:14:43 +0100

    Initial commit

---
+++ app/src/main/java/com/curiouswizard/marsrealestate/detail/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.curiouswizard.marsrealestate.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.curiouswizard.marsrealestate.databinding.FragmentDetailBinding
+
+/**
+ * This [Fragment] will show the detailed information about a selected piece of Mars real estate.
+ */
+class DetailFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        @Suppress("UNUSED_VARIABLE")
+        val application = requireNotNull(activity).application
+        val binding = FragmentDetailBinding.inflate(inflater)
+
+        val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+
+        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+        binding.viewModel = ViewModelProvider(
+            this, viewModelFactory).get(DetailViewModel::class.java)
+
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/curiouswizard/marsrealestate/network/MarsProperty.kt
@@ -0,0 +1,15 @@
+package com.curiouswizard.marsrealestate.network
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MarsProperty(
+        val id: String,
+        @Json(name = "img_src") val imgSrcUrl: String,
+        val type: String,
+        val price: Double
+): Parcelable{
+        val isRental = type == "rent"
+}
